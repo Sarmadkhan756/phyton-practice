42e7d242717ff028e2a7d8a952cd509a1f7a11b6
Sarmadkhan756 <ksarmad713@gmail.com>
2024-08-28 23:53:52 -0700

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,26 @@
+# patient_name= "John Smith"
+# patient_age = 39
+# patient_new = True
+# print(patient_name)
+# print(patient_age)
+# print(patient_new)
+
+# college = input("what is your college name? ")
+# print("ghark sha pa " + college + " ke") 
+
+# This is a program to add 2 numbers
+
+# num1 = str(input("input your first number:- "))
+# num2= str(input("input your second number:- "))
+# print(num1 + num2)
+
+# user_age = input("enter the year of birth:- ")
+# age = 2024 - int(user_age)
+# print(age)
+
+str()
+bool()
+int()
+float()
+
+
